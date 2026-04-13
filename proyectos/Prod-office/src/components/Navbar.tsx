8db83951e4be79ff4e2d70d3af8926d21caec544
EmanuelQCh <153540858+EmanuelQCh@users.noreply.github.com>
2026-04-12 22:09:19 -0400

    Create Navbar.tsx

---
+++ proyectos/Prod-office/src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav>
+      <h1>Prod Office</h1>
+
+      <ul>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Servicios</a></li>
+        <li><a href="#">Contacto</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
