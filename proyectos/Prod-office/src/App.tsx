6a765e7a1ab641cc9d8adfb9c5b7228144825bd8
EmanuelQCh <153540858+EmanuelQCh@users.noreply.github.com>
2026-04-12 22:11:14 -0400

    App.tsx

--- proyectos/Prod-office/src/App.tsx
+++ proyectos/Prod-office/src/App.tsx
@@ -1,5 +1,9 @@
 import AppRouter from "./routes/AppRouter";
-
+import Navbar from './components/Navbar'
 export default function App() {
   return <AppRouter />;
+   <div>
+        <Navbar />
+        <h2>Bienvenido</h2>
+      </div>
 }
