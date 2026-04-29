c4d93bb55a1dc4418a31434b4be68986615b5fa4
EmanuelQCh <153540858+EmanuelQCh@users.noreply.github.com>
2026-04-29 12:08:55 -0400

    Delete Documents/Proyecto-TAW-Comienzo_nuevo/Proyecto-TAW-Comienzo_nuevo/proyectos/AURA NOVA directory

--- Documents/Proyecto-TAW-Comienzo_nuevo/Proyecto-TAW-Comienzo_nuevo/proyectos/AURA NOVA/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import AppRouter from "./app/routes/AppRouter";
-
-function App() {
-  return <AppRouter />;
-}
-
-export default App;
--- Documents/Proyecto-TAW-Comienzo_nuevo/Proyecto-TAW-Comienzo_nuevo/proyectos/AURA NOVA/src/pages/LoginPage.tsx
+++
@@ -1,5 +0,0 @@
-const LoginPage = () => {
-  return <h1 className="text-3xl font-bold">Login</h1>;
-};
-
-export default LoginPage;
