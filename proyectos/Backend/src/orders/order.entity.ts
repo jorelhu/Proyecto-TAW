2f06d634dcffd41842ea43b73bc75b0520661557
jorelhu <humildejorel@gmail.com>
2026-06-11 00:24:03 -0400

    si

--- proyectos/Backend/src/orders/order.entity.ts
+++ proyectos/Backend/src/orders/order.entity.ts
@@ -35,6 +35,6 @@ export class Order {
   @JoinColumn({ name: 'userId' })
   user: Usuario;
 
-  @OneToMany(() => OrderItem, (orderItem) => orderItem.order)
-  items: OrderItem[];
+  @OneToMany(() => OrderItem, (item) => item.order, { cascade: true })
+  items!: OrderItem[];
 }
--- proyectos/Backend/src/product-variants/product-variant.entity.ts
+++ proyectos/Backend/src/product-variants/product-variant.entity.ts
@@ -20,9 +20,6 @@ export class ProductVariant {
   @Column({ length: 100 })
   size!: string;
 
-  //@Column({ length: 50 })
-  //concentration!: string;
-
   @Column('decimal', { precision: 10, scale: 2 })
   price!: number;
 
